d8dfd7ac9222c8531cc87e8b46dbcbe93aa14e17
Ste <s.o'neill@brassagency.com>
2018-08-06 11:43:39 +0100

    park test

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express'),
+app = express(),
+axios = require('axios'),
+themeparks = require('themeparks'),
+moment = require('moment');
+
+
+app.get('/parks/:parkId', (req, res) => {
+    let park = new themeparks.Parks[parkId];
+    park.GetWaitTimes().then(function(rides) {
+        // print each wait time
+        for(var i=0, ride; ride=rides[i++];) {
+            console.log(ride.name + ": " + ride.waitTime + " minutes wait");
+        }
+    }, console.error);
+
+})
+
+app.listen(process.env.PORT, () => {
+    console.log('app listening on ' + process.end.PORT)
+}
